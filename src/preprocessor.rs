d167322ae7c4a2993b700a1b2b94f6b791136c7f
Armin Häberling <armin.aha@gmail.com>
2021-06-06 22:04:16 +0200

    Simplify code and fix some clippy errors

--- src/preprocessor.rs
+++ src/preprocessor.rs
@@ -10,21 +10,27 @@ impl CurlyQuotes {
         CurlyQuotes
     }
 
-    fn process_item(&self, item: &mut BookItem) -> Result<(), Error> {
+    fn process_item(item: &mut BookItem) -> Result<(), Error> {
         if let BookItem::Chapter(ref mut ch) = item {
-            ch.content = self.process_content(&ch.content)?;
+            ch.content = CurlyQuotes::process_content(&ch.content)?;
         }
         Ok(())
     }
 
-    fn process_content(&self, content: &str) -> Result<String, Error> {
+    fn process_content(content: &str) -> Result<String, Error> {
         let parser = mdbook::utils::new_cmark_parser(content);
-        let mut converter = EventQuoteConverter::new();
-        let events = parser.map(|event| converter.convert(event));
+        let events = parser.map(CurlyQuotes::convert_event);
         let mut buffer = String::new();
         pulldown_cmark_to_cmark::cmark(events, &mut buffer, None)
             .map_err(|err| Error::new(err).context("Markdown serialization failed"))?;
         Ok(buffer)
+    }
+
+    fn convert_event(event: Event) -> Event {
+        match event {
+            Event::Text(ref text) => Event::Text(CowStr::from(convert_quotes_to_curly(text))),
+            _ => event,
+        }
     }
 }
 
@@ -36,32 +42,13 @@ impl Preprocessor for CurlyQuotes {
     fn run(&self, _ctx: &PreprocessorContext, mut book: Book) -> Result<Book, Error> {
         let mut err = None;
         book.for_each_mut(|item| {
-            self.process_item(item).unwrap_or_else(|e| {
+            CurlyQuotes::process_item(item).unwrap_or_else(|e| {
                 if err.is_none() {
                     err = Some(e);
                 }
             })
         });
-        if let Some(err) = err {
-            Err(err)
-        } else {
-            Ok(book)
-        }
-    }
-}
-
-struct EventQuoteConverter;
-
-impl EventQuoteConverter {
-    fn new() -> Self {
-        EventQuoteConverter
-    }
-
-    fn convert<'a>(&mut self, event: Event<'a>) -> Event<'a> {
-        match event {
-            Event::Text(ref text) => Event::Text(CowStr::from(convert_quotes_to_curly(text))),
-            _ => event,
-        }
+        err.map_or(Ok(book), Err)
     }
 }
 
@@ -104,10 +91,8 @@ mod test {
 
     #[test]
     fn process_content() {
-        let preprocessor = CurlyQuotes::new();
-        let new_content = preprocessor
-            .process_content("[\"example\"](https://www.rust-lang.org/\")")
-            .unwrap();
+        let new_content =
+            CurlyQuotes::process_content("[\"example\"](https://www.rust-lang.org/\")").unwrap();
         assert_eq!(new_content, "[“example”](https://www.rust-lang.org/\")")
     }
 }
